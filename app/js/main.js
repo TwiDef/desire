9559670df408de22378d8d62cf158ad5709031cd
Def <deferenseo@gmail.com>
2023-01-15 14:55:22 +0300

    added functional(open-close) to rightside menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
-console.log('Lorem');
+$(function() {
+
+    $('.header__btn').on('click', function() {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+    $('.rightside-menu__close').on('click', function() {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+
+});
