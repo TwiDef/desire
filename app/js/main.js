f0caf5bb5765f7713ff05080f6e8ecab4362509c
Def <deferenseo@gmail.com>
2023-02-18 18:06:03 +0300

    done second blog

--- app/js/main.js
+++ app/js/main.js
@@ -23,6 +23,17 @@ $(function() {
         autoplaySpeed: 3000
     });
 
+    $('.article-slider__box').slick({
+        arrows: true,
+        /* fade: true */
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/slider/back.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/slider/next.svg" alt="right"></button>',
+    });
+
+
+
 
     const containerEl = document.querySelector('.gallery__inner');
     let mixer;
